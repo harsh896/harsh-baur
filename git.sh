9d79387114c8f683aeb97ef6838a2b13a1056f6d
harsh896 <hingrajiaharsh896@gmail.com>
2020-07-15 10:09:55 +0530

    commit changes

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git add .
-echo "\[\033[1;31m\] Adding all files"
+echo -e "\e[1;31m Adding all files"
 git commit -m "commit changes"
-echo "\[\033[1;33m\] Commiting git"
+echo -e "\e[1;33m Commiting git"
 git push origin master
-echo "\[\033[1;32m\] Pushing to git repo"
+echo -e "\e[1;32m Pushing to git repo"
