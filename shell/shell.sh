43e9dbd470d4add421aa5849533a9a933f706043
neeraj.j.addweb@gmail.com <neeraj.j.addweb@gmail.com>
2020-07-16 10:32:03 +0530

    commit changes

---
+++ shell/shell.sh
@@ -0,0 +1,9 @@
+Name=Harsh
+Age=23
+Work="DevOps Engineer"
+Company="Addweb Solutions"
+
+echo "Hi my name is $Name."
+echo "I am $Age years old."
+echo "I am working as a $Work in $Company."
+echo "Have a nice day"
